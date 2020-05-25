9141dbeb6254435ffd103f8aedbf84aafe51b506
Marcel S. Gongora <2498591+msgongora@users.noreply.github.com>
2020-05-25 05:37:22 -0400

    Migrate scripts to python3

--- package_manager/dpkg_parser.py
+++ package_manager/dpkg_parser.py
@@ -12,29 +12,29 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import argparse
+import io
+import os
 import gzip
 import json
-import os
-import io
+import argparse
+from urllib.request import Request, urlopen
 
-from six.moves import urllib
-
-from package_manager.parse_metadata import parse_package_metadata
 from package_manager import util
+from package_manager.parse_metadata import parse_package_metadata
 
 OUT_FOLDER = "file"
 OS_RELEASE_PATH = "etc"
-PACKAGES_FILE_NAME = os.path.join(OUT_FOLDER,"Packages.json")
-PACKAGE_MAP_FILE_NAME = os.path.join(OUT_FOLDER,"packages.bzl")
+PACKAGES_FILE_NAME = os.path.join(OUT_FOLDER, "Packages.json")
+PACKAGE_MAP_FILE_NAME = os.path.join(OUT_FOLDER, "packages.bzl")
 OS_RELEASE_FILE_NAME = os.path.join(OS_RELEASE_PATH, "os-release")
 OS_RELEASE_TAR_FILE_NAME = os.path.join(OUT_FOLDER, "os_release.tar")
-DEB_FILE_NAME = os.path.join(OUT_FOLDER,"pkg.deb")
+DEB_FILE_NAME = os.path.join(OUT_FOLDER, "pkg.deb")
 
 FILENAME_KEY = "Filename"
 SHA256_KEY = "SHA256"
 VERSION_KEY = "Version"
 
+# fmt: off
 parser = argparse.ArgumentParser(
     description="Downloads a deb package from a package source file"
 )
@@ -62,17 +62,28 @@ parser.add_argument("--packages-gz-url", action='store',
                     help='The full url for the Packages.gz file')
 parser.add_argument("--package-prefix", action='store',
                     help='The prefix to prepend to the value of Filename key in the Packages.gz file.')
+# fmt: on
 
 
 def main():
     """ A tool for downloading debian packages and package metadata """
     args = parser.parse_args()
     if args.download_and_extract_only:
-        download_package_list(args.mirror_url,args.distro, args.arch, args.snapshot, args.sha256,
-                              args.packages_gz_url, args.package_prefix)
-        util.build_os_release_tar(args.distro, OS_RELEASE_FILE_NAME, OS_RELEASE_PATH, OS_RELEASE_TAR_FILE_NAME)
+        download_package_list(
+            args.mirror_url,
+            args.distro,
+            args.arch,
+            args.snapshot,
+            args.sha256,
+            args.packages_gz_url,
+            args.package_prefix,
+        )
+        util.build_os_release_tar(
+            args.distro, OS_RELEASE_FILE_NAME, OS_RELEASE_PATH, OS_RELEASE_TAR_FILE_NAME
+        )
     else:
         download_dpkg(args.package_files, args.packages, args.workspace_name)
+
 
 def download_dpkg(package_files, packages, workspace_name):
     """ Using an unzipped, json package file with full urls,
@@ -81,7 +92,7 @@ def download_dpkg(package_files, packages, workspace_name):
     Uses the 'Filename' key to download the .deb package
     """
     pkg_vals_to_package_file_and_sha256 = {}
-    package_to_rule_map = {}
+    util.package_to_rule_map = {}
     package_to_version_map = {}
     package_file_to_metadata = {}
     for pkg_vals in set(packages.split(",")):
@@ -93,41 +104,57 @@ def download_dpkg(package_files, packages, workspace_name):
             pkg_name, pkg_version = pkg_split
         for package_file in package_files.split(","):
             if package_file not in package_file_to_metadata:
-                with open(package_file, 'rb') as f:
+                with open(package_file, "rb") as f:
                     data = f.read()
-                    package_file_to_metadata[package_file] = json.loads(data.decode('utf-8'))
+                    package_file_to_metadata[package_file] = json.loads(data.decode("utf-8"))
             metadata = package_file_to_metadata[package_file]
-            if (pkg_name in metadata and
-            (pkg_version == "" or
-            pkg_version == metadata[pkg_name][VERSION_KEY])):
+            if pkg_name in metadata and (
+                pkg_version == "" or pkg_version == metadata[pkg_name][VERSION_KEY]
+            ):
                 pkg = metadata[pkg_name]
                 out_file = os.path.join("file", util.encode_package_name(pkg_name))
                 download_and_save(pkg_name, pkg[FILENAME_KEY], out_file)
-                package_to_rule_map[pkg_name] = util.package_to_rule(workspace_name, pkg_name)
+                package_to_rule_map[pkg_name] = package_to_rule(workspace_name, pkg_name)
                 package_to_version_map[pkg_name] = metadata[pkg_name][VERSION_KEY]
                 actual_checksum = util.sha256_checksum(out_file)
                 expected_checksum = pkg[SHA256_KEY]
                 if actual_checksum != expected_checksum:
-                    raise Exception("Wrong checksum for package %s (%s).  Expected: %s, Actual: %s" %(pkg_name, pkg[FILENAME_KEY], expected_checksum, actual_checksum))
+                    raise Exception(
+                        "Wrong checksum for package %s (%s).  Expected: %s, Actual: %s"
+                        % (pkg_name, pkg[FILENAME_KEY], expected_checksum, actual_checksum)
+                    )
                 if pkg_version == "":
                     break
-                if (pkg_vals in pkg_vals_to_package_file_and_sha256 and
-                pkg_vals_to_package_file_and_sha256[pkg_vals][1] != actual_checksum):
-                    raise Exception("Conflicting checksums for package %s, version %s.  Conflicting checksums: %s:%s, %s:%s" %
-                    (pkg_name, pkg_version,
-                     pkg_vals_to_package_file_and_sha256[pkg_vals][0], pkg_vals_to_package_file_and_sha256[pkg_vals][1],
-                     package_file, actual_checksum))
+                if (
+                    pkg_vals in pkg_vals_to_package_file_and_sha256
+                    and pkg_vals_to_package_file_and_sha256[pkg_vals][1] != actual_checksum
+                ):
+                    raise Exception(
+                        "Conflicting checksums for package %s, version %s.  Conflicting checksums: %s:%s, %s:%s"
+                        % (
+                            pkg_name,
+                            pkg_version,
+                            pkg_vals_to_package_file_and_sha256[pkg_vals][0],
+                            pkg_vals_to_package_file_and_sha256[pkg_vals][1],
+                            package_file,
+                            actual_checksum,
+                        )
+                    )
                 else:
                     pkg_vals_to_package_file_and_sha256[pkg_vals] = [package_file, actual_checksum]
                 break
         else:
-            raise Exception("Package: %s, Version: %s not found in any of the sources" % (pkg_name, pkg_version))
-    with open(PACKAGE_MAP_FILE_NAME, 'w') as f:
+            raise Exception(
+                "Package: %s, Version: %s not found in any of the sources"
+                % (pkg_name, pkg_version)
+            )
+    with open(PACKAGE_MAP_FILE_NAME, "w") as f:
         f.write("packages = " + json.dumps(package_to_rule_map))
         f.write("\nversions = " + json.dumps(package_to_version_map))
 
+
 def download_and_save(pkg_key, url, out_file, retry_count=20):
-    res = urllib.request.urlopen(url)
+    res = urlopen(url)
     remaining_bytes = int(res.info().getheader("Content-Length"))
     downloaded = res.read()
     contents = []
@@ -139,14 +166,19 @@ def download_and_save(pkg_key, url, out_file, retry_count=20):
         range_access_enabled = "bytes" in res.info().getheader("Accept-Ranges")
         etag = res.info().getheader("ETag")
         if not range_access_enabled:
-            raise Exception("Fail to download %s (%s). Server returned partial contents." %(pkg_key, url))
+            raise Exception(
+                "Fail to download %s (%s). Server returned partial contents." % (pkg_key, url)
+            )
 
         while retry_count > 0:
             retry_count -= 1
-            req = urllib.request.Request(url, headers={"Range": "bytes=%d-" % offset, "If-Range": etag})
-            res = urllib.request.urlopen(req)
+            req = Request(url, headers={"Range": "bytes=%d-" % offset, "If-Range": etag})
+            res = urlopen(req)
             if res.getcode() != 206:
-                raise Exception("Fail to download %s (%s). Server did not return '206 Partial Content'" %(pkg_key, url))
+                raise Exception(
+                    "Fail to download %s (%s). Server did not return '206 Partial Content'"
+                    % (pkg_key, url)
+                )
             downloaded = res.read()
             contents.append(downloaded)
             remaining_bytes -= len(downloaded)
@@ -154,12 +186,17 @@ def download_and_save(pkg_key, url, out_file, retry_count=20):
             if remaining_bytes == 0:
                 break
         if remaining_bytes != 0:
-            raise Exception("Fail to download %s (%s). Too many Range request retries." %(pkg_key, url))
-    with io.open(out_file, 'wb') as f:
+            raise Exception(
+                "Fail to download %s (%s). Too many Range request retries." % (pkg_key, url)
+            )
+    with io.open(out_file, "wb") as f:
         for c in contents:
             f.write(c)
 
-def download_package_list(mirror_url, distro, arch, snapshot, sha256, packages_gz_url, package_prefix):
+
+def download_package_list(
+    mirror_url, distro, arch, snapshot, sha256, packages_gz_url, package_prefix
+):
     """Downloads a debian package list, expands the relative urls,
     and saves the metadata as a json file
 
@@ -191,11 +228,15 @@ SHA256: 52ec3ac93cf8ba038fbcefe1e78f26ca1d59356cdc95e60f987c3f52b3f5e7ef
     """
 
     if bool(packages_gz_url) != bool(package_prefix):
-        raise Exception("packages_gz_url and package_prefix must be specified or skipped at the same time.")
+        raise Exception(
+            "packages_gz_url and package_prefix must be specified or skipped at the same time."
+        )
 
     if (not packages_gz_url) and (not mirror_url or not snapshot or not distro or not arch):
-        raise Exception("If packages_gz_url is not specified, all of mirror_url, snapshot, "
-                        "distro and arch must be specified.")
+        raise Exception(
+            "If packages_gz_url is not specified, all of mirror_url, snapshot, "
+            "distro and arch must be specified."
+        )
 
     url = packages_gz_url
     if not url:
@@ -203,20 +244,23 @@ SHA256: 52ec3ac93cf8ba038fbcefe1e78f26ca1d59356cdc95e60f987c3f52b3f5e7ef
             mirror_url,
             snapshot,
             distro,
-            arch
+            arch,
         )
 
-    buf = urllib.request.urlopen(url)
-    with io.open("Packages.gz", 'wb') as f:
+    buf = urlopen(url)
+    with io.open("Packages.gz", "wb") as f:
         f.write(buf.read())
     actual_sha256 = util.sha256_checksum("Packages.gz")
     if sha256 != actual_sha256:
-        raise Exception("sha256 of Packages.gz don't match: Expected: %s, Actual:%s" %(sha256, actual_sha256))
-    with gzip.open("Packages.gz", 'rb') as f:
+        raise Exception(
+            "sha256 of Packages.gz don't match: Expected: %s, Actual:%s" % (sha256, actual_sha256)
+        )
+    with gzip.open("Packages.gz", "rb") as f:
         data = f.read()
     metadata = parse_package_metadata(data, mirror_url, snapshot, package_prefix)
-    with open(PACKAGES_FILE_NAME, 'w') as f:
+    with open(PACKAGES_FILE_NAME, "w") as f:
         json.dump(metadata, f)
+
 
 if __name__ == "__main__":
     main()
--- package_manager/parse_metadata.py
+++ package_manager/parse_metadata.py
@@ -13,19 +13,18 @@
 # limitations under the License.
 import re
 
-import six
-
 INDEX_KEY = "Package"
 FILENAME_KEY = "Filename"
 SEPARATOR = ":"
+
 
 def parse_package_metadata(data, mirror_url, snapshot, package_prefix):
     """ Takes a debian package list, changes the relative urls to absolute urls,
     and saves the resulting metadata as a json file """
 
     # this is called with the output of gzip.open, but should be strings
-    if isinstance(data, six.binary_type):
-        data = data.decode('utf-8')
+    if isinstance(data, bytes):
+        data = data.decode("utf-8")
 
     raw_entries = [line.rstrip() for line in data.splitlines()]
     parsed_entries = {}
@@ -36,7 +35,7 @@ def parse_package_metadata(data, mirror_url, snapshot, package_prefix):
         if line:
             # If the line starts with indentation,
             # it is a continuation of the previous key
-            if re.match(r'\s', line):
+            if re.match(r"\s", line):
                 if current_entry is None or current_key is None:
                     raise Exception("Found incorrect indention on line:" + line)
                 current_entry[current_key] += line.strip()
@@ -44,12 +43,12 @@ def parse_package_metadata(data, mirror_url, snapshot, package_prefix):
                 (key, value) = line.split(SEPARATOR, 1)
                 current_key = key.strip()
                 if current_key in current_entry:
-                    raise Exception("Duplicate key for package metadata:"
-                                    + current_key + "\n" + current_entry)
+                    raise Exception(
+                        "Duplicate key for package metadata:" + current_key + "\n" + current_entry
+                    )
                 current_entry[current_key] = value.strip()
             else:
-                raise Exception("Valid line, but no delimiter or indentation:"
-                                + line)
+                raise Exception("Valid line, but no delimiter or indentation:" + line)
         else:
             if current_entry:
                 parsed_entries[current_entry[INDEX_KEY]] = current_entry
@@ -60,9 +59,11 @@ def parse_package_metadata(data, mirror_url, snapshot, package_prefix):
     # The Filename Key is a relative url pointing to the .deb package
     # Here, we're rewriting the metadata with the absolute urls,
     # which is a concatenation of the mirror + '/debian/' + relative_path
-    for pkg_data in six.itervalues(parsed_entries):
+    for pkg_data in parsed_entries:
         if package_prefix:
-            pkg_data[FILENAME_KEY] = package_prefix + pkg_data[FILENAME_KEY]
+            parsed_entries[pkg_data] = package_prefix + parsed_entries[pkg_data][FILENAME_KEY]
         else:
-            pkg_data[FILENAME_KEY] = mirror_url + "/debian/" + snapshot + "/" + pkg_data[FILENAME_KEY]
+            parsed_entries[pkg_data] = (
+                mirror_url + "/debian/" + snapshot + "/" + parsed_entries[pkg_data][FILENAME_KEY]
+            )
     return parsed_entries
